2f30ec89163e5c1fc23491c30359dd2abad689e0
wengxiaojun <xiaojun@smartx.com>
2021-06-11 16:56:01 +0800

    init infrustructure

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import external from 'rollup-plugin-peer-deps-external';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.DEVELOPMENT;
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: './build/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      external(),
+      resolve({ browser: true, preferBuiltins: true }),
+      typescript(),
+      production && terser(),
+    ],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      name: 'recorder',
+      file: './build/index.browser.js',
+      format: 'iife',
+      sourcemap: true,
+    },
+    plugins: [
+      commonjs(),
+      external(),
+      resolve({ browser: true, preferBuiltins: true }),
+      typescript(),
+    ],
+  },
+];
